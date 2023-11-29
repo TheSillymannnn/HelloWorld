5084fbdfd2a24b919ae654f809ce38a9f5b62b9d
TheSillymannnn <151578228+TheSillymannnn@users.noreply.github.com>
2023-11-29 18:32:04 -0500

    first edit

---
+++ main.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class Main
+{
+    public static void Main (String[] args)
+    {
+        int x =0;
+        int y =0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome! to my Deathcube!tm ");
+        System.out.println("you are at position [0,0]");
+        System.out.println("Your at " + "PUT THE RANDOM LOCATION HERE");
+        while
+            System.out.println("Which direaction do you want to go in?");
+            String direction = scan.nextLine();
+            if(direction.equals ("right")){
+                // CALL A FUCTION THAT ADDS ONE TO X VALUE 
+            }
+            else if (direaction.equals("left")){
+                // CALL FUCTION THAT SUBTRACTS ONE FROM X VALUE 
+            }
+            else if (direaction.equals("up")){
+                // CALL FUCTION THAT ADDS ONE FROM Y VALUE 
+                
+            }
+            else if (direaction.equals("down")){
+                // CALL FUCTION THAT SUBTRACTS ONE FROM Y VALUE 
+            }
+
+            else if(//CALL A HOLE FUNCTION USING COORDANATES//  
+            {
+                System.out.Println("You lose!");
+                break;
+            }
+            
+            else if(CALL ENDGOAL FUNCTION USING COORDANATES){
+                System.out.Println("You win!");
+                break;
+            }
+            else if(CALL ROCK FUNCTION USING COORDANATES){
+
+            }
+
+            else if(CALL X OUT OF BOUNDS FUNCTION USING COORDANATES){
+                System.out.println("invalid move");
+            }
+
+            else if(CALL Y OUT OF BOUNDS FUNCTION USING COORDANATES){
+
+            }
+
+            else if(CALL X OUT OF BOUNDS FUNCTION USING COORDANATES){
+
+            }
+            else{
+                System.out.println("ok thats valid");
+            }
+
+            System.out.println("you are at position (" + x + ", " + y + ")");
+            System.out.println("")
+
+
+    }
+}
